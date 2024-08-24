d9693bc87f4bcbff54a2c763f8d641ccf922ca6a
oalvesj <116018283+oalvesj@users.noreply.github.com>
2024-08-24 12:07:35 +0100

    Add files via upload

---
+++ hellofunction.js
@@ -0,0 +1,10 @@
+/**
+* JS - Função Simples | Exercício de Fixação
+* @author Osni Alves Junior
+*/
+
+function hello(){
+	var nome;
+	nome = prompt("Qual é o seu nome?");
+	alert("Hello " + nome);
+}
